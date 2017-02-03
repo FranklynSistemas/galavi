96280010c71d81fe7c0ac2b87d58fb0bbd1344cf
FranklynSistemas <Franking.sistemas@gmail.com>
2017-02-02 20:44:49 -0500

    videos

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,5 +49,105 @@ wow.init();
 		e.preventDefault();
 		$body.animate({scrollTop:h+80}, '6000');
 	});
+
+	function isTouchDevice(){
+		if ("ontouchstart" in document.documentElement)
+		{
+		  // It's a touch screen device.
+		  return true;
+		}
+		else
+		{
+		  // Others devices.
+		  return false;
+		}
+	}
+	
+	function touchScroll(id){
+		if(isTouchDevice()){ //if touch events exist...
+			var el=document.getElementById(id);
+			var scrollStartPos=0;
+
+			document.getElementById(id).addEventListener("touchstart", function(event) {
+				scrollStartPos=this.scrollTop+event.touches[0].pageY;
+				event.preventDefault();
+			},false);
+
+			document.getElementById(id).addEventListener("touchmove", function(event) {
+				this.scrollTop=scrollStartPos-event.touches[0].pageY;
+				event.preventDefault();
+			},false);
+		}
+	}
+
+	$( document ).ready(function() {
+
+    scaleVideoContainer();
+
+    initBannerVideoSize('.video-container .poster img');
+    initBannerVideoSize('.video-container .filter');
+    initBannerVideoSize('.video-container video');
+
+    $(window).on('resize', function() {
+        scaleVideoContainer();
+        scaleBannerVideoSize('.video-container .poster img');
+        scaleBannerVideoSize('.video-container .filter');
+        scaleBannerVideoSize('.video-container video');
+    });
+
+});
+
+function scaleVideoContainer() {
+
+    var height = $(window).height() + 5;
+    var unitHeight = parseInt(height) + 'px';
+    $('.homepage-hero-module').css('height',unitHeight);
+
+}
+
+function initBannerVideoSize(element){
+
+    $(element).each(function(){
+        $(this).data('height', $(this).height());
+        $(this).data('width', $(this).width());
+    });
+
+    scaleBannerVideoSize(element);
+
+}
+
+function scaleBannerVideoSize(element){
+
+    var windowWidth = $(window).width(),
+    windowHeight = $(window).height() + 5,
+    videoWidth,
+    videoHeight;
+
+    console.log(windowHeight);
+
+    $(element).each(function(){
+        var videoAspectRatio = $(this).data('height')/$(this).data('width');
+
+        $(this).width(windowWidth);
+
+        if(windowWidth < 1000){
+            videoHeight = windowHeight;
+            videoWidth = videoHeight / videoAspectRatio;
+            $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+
+            $(this).width(videoWidth).height(videoHeight);
+        }
+
+        $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+
+    });
+}
+	//touchScroll("page");
+	/*$(document).ready(function() {
+		$("#oneAcerca").smoothTouchScroll({ 
+			continuousScrolling: true
+		});
+	});*/
+
 	
 })
